e4d0095958d7a16c0e277b1314e5dfebd839940b
xingkeruan <xingkeruan@Xingkes-MacBook-Air.local>
2024-02-04 13:26:31 -0800

    buffer 和 zip的使用

--- src/test/java/StreamOpsTest.java
+++ src/test/java/StreamOpsTest.java
@@ -3,8 +3,10 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.SignalType;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Slf4j
@@ -77,5 +79,34 @@ public class StreamOpsTest {
             log.info("always execute");
             super.hookFinally(type);
         }
+    }
+    @Test
+    void streamBuffer() {
+        Flux<Integer> flux = Flux.range(1, 40);
+        // 会将每3个元素作为一组进行传递
+        Flux<List<Integer>> buffer = flux.buffer(3);
+        buffer.log().subscribe();
+    }
+
+    @Test
+    void streamRetry() {
+        Mono<String> client = Mono.fromSupplier(() -> {
+            double num = Math.random();
+            if(num > 0.05) {
+                throw new Error("error network");
+            }
+            return "https://www.google.com";
+        });
+        client.log().retry(3).subscribe();
+    }
+
+    @Test
+    void streamZip() {
+        Flux<Integer> fluxA = Flux.range(1,4);
+        Flux<Integer> fluxB = Flux.range(5,8);
+        fluxA.zipWith(fluxB, (a, b) -> a + b)
+                .log()
+                .subscribe();
+
     }
 }
